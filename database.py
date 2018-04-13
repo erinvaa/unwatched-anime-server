d92088497cd17dd7ba385fc451f1fe9f2665f62f
erinvaa <erin.vaa@gmail.com>
2018-04-13 12:24:19 -0400

    Added database support for custom urls for users

--- database.py
+++ database.py
@@ -29,8 +29,29 @@ class VideoSource(db.Model):
     country_id = db.Column(db.Integer, db.ForeignKey('countries.id'), nullable=False)
 
 
+class CustomVideoSource(db.Model):
+    __tablename__ = 'custom_video_source'
+
+    id = db.Column(db.Integer, primary_key=True)
+    mal_id = db.Column(db.Integer, nullable=False)
+    url = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    video_source_type = db.Column(db.Integer, db.ForeignKey('video_source_types.id'), nullable=True)
+
+
 class SkippedEpisodes(db.Model):
     __tablename__ = 'skipped_episodes'
 
     mal_id = db.Column(db.Integer, primary_key=True)
     skipped_episodes = db.Column(db.Integer, nullable=False)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    mal_name = db.Column(db.String, nullable=False)
+    default_country_id = db.Column(db.Integer, db.ForeignKey('countries.id'), nullable=True)
+
+
+
